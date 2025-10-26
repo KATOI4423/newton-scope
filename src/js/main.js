69a39cf10aeb209be033e7f676fa9931216db285
KATOI4423 <katoi4423@gmail.com>
2025-10-26 17:39:04 +0900

    js: Move shader function into shader.js
    shader関連の関数定義をmain.jsからshader.jsに移動する

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,6 @@
 // main.js
 
-import { vertexShaderSource, fragmentShaderSource } from "./shader.js";
-
-function createShader(gl, type, source) {
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.error(gl.getShaderInfoLog(shader));
-        gl.deleteShader(shader);
-        return null;
-    }
-    return shader;
-}
-
-function createProgram(gl, vsSource, fsSource) {
-    const vs = createShader(gl, gl.VERTEX_SHADER, vsSource);
-    const fs = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
-    const program = gl.createProgram();
-    gl.attachShader(program, vs);
-    gl.attachShader(program, fs);
-    gl.linkProgram(program);
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.error(gl.getProgramInfoLog(program));
-        return null;
-    }
-    return program;
-}
+import { vertexShaderSource, fragmentShaderSource, createProgram } from "./shader.js";
 
 function main() {
     const canvas = document.getElementById("fractal");
--- src/js/shader.js
+++ src/js/shader.js
@@ -89,3 +89,29 @@ void main() {
     // outColor = jet(log(iter + 1.0) / log(float(u_repmax) + 1.0));
 }
 `;
+
+function createShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+    return shader;
+}
+
+export function createProgram(gl, vsSource, fsSource) {
+    const vs = createShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fs = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    const program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error(gl.getProgramInfoLog(program));
+        return null;
+    }
+    return program;
+}
